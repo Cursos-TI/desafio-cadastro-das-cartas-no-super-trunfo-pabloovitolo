a501d8ab2965d8581f879b86366d8af491c8d877
PABLO VITOLO <pablovitolo1378@gmail.com>
2025-03-06 11:58:10 -0300

    C:\Users\Vitolo\OneDrive\Documentos\Faculdade\DesafioSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+    int main(){
+        printf ("Desafio Super Trunfo - Países")
+    
+    
+    
+    }
+
+
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
